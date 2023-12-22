6839acc109eae3fea37ec5b3c355bf66a3afa975
MARIEM0123 <129907944+MARIEM0123@users.noreply.github.com>
2023-12-22 14:01:37 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 
 
 class User(BaseModel, Base):
